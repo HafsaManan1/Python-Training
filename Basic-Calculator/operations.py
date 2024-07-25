14ef84515c498e45b6f441a137cd06a787a808b9
HafsaManan1 <hafsa.manan@osoltech.com>
2024-07-25 15:18:09 +0500

    Implemented suggested changes from code review

--- Basic-Calculator/operations.py
+++ Basic-Calculator/operations.py
@@ -9,15 +9,15 @@ def addition():
 
 def subtraction():
     num1,num2 = number_input()
-    return f"\nResult: {num1}+{num2} = {num1-num2}\n"
+    return f"\nResult: {num1}-{num2} = {num1-num2}\n"
 
 def multiplication():
     num1,num2 = number_input()
-    return f"\nResult: {num1}+{num2} = {num1*num2}\n"
+    return f"\nResult: {num1}x{num2} = {num1*num2}\n"
 
 def division():
     num1,num2 = number_input()
     if num2==0:
         return ("\nDivision by zero is invalid\n")
     else:    
-        return f"\nResult: {num1}+{num2} = {num1/num2}\n"
+        return f"\nResult: {num1}÷{num2} = {num1/num2}\n"
